aa156d6a0f2e8cd1d0733f126259363085e5a83c
Geilson <geilsonsrz@gmail.com>
2024-01-06 17:55:41 -0300

    Funcao de superficie

--- src/scripts/processamento/f_graficando.js
+++ src/scripts/processamento/f_graficando.js
@@ -2,40 +2,72 @@
     Função para graficar os dados das caixas
 `
 
-
 // Gerar gráficos
 function graficos(dados) {
 
     // Identificação da seção campo de plotagem
     const area_resultados = d3.select('#resultados')
 
-
-        // Identificação do link para os gráficos
-        const link_graficos = document.getElementById('link-graficos')
-
-        // Tornando visível ao primeiro clique.
-        document.getElementById('resultados').classList.remove('oculto')
-        document.getElementById('link-graficos').classList.remove('oculto')
+    // Tornando visível ao primeiro clique.
+    document.getElementById('resultados').classList.remove('oculto')
+    document.getElementById('link-graficos').classList.remove('oculto')
 
 
-        // Para cada elemento dos dados, gerar um gráfico
-        for (let chave in dados) {
+    // Especificando as funções para cada chaves
+    const funcoesChaves = {
+
+        'caixa01': (dado) => {},
+
+
+        'caixa02': (dado) => {},
+
+
+        'caixa03': (dado) => {},
+
+
+        'umidade': (dado) => {},
+
+
+        'superficie': (dado) => {
+
+            const temperaturas = []
+            const momentos = []
+
+            for (let i in dado) {
             
-            // Criação da divisão do gráfico
-            area_resultados.append('div')
-                .attr('class', 'conteiner-grafico')
-                .text(`${chave}`)
+                temperaturas.push(dado[i][0])
 
-
-            for (let i in dados[chave]) {
-
-                let dado = dados[chave][i][0]
-
+                momentos.push(i)
+            
             }
+
+            console.log(temperaturas)
+            console.log(momentos)
+
+            let dados = momentos.map(function(d, i) {
+                return { momento: d, temperatura: temperaturas[i] };
+            });
+
+
 
         }
 
+    }
+
+
+    // Para cada elemento dos dados, gerar um gráfico
+    for (let chave in dados) {
+
+        // Criação da divisão do gráfico
+        area_resultados.append('div')
+            .attr('class', 'conteiner-grafico')
+            .text(`${chave}`)
+
+        funcoesChaves[chave](dados[chave])
         
+    }
+
+
 }
 
 
