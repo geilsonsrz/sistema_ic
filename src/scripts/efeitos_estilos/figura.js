26533de3845dddf19e636847460cfbb69baff5c6
Geilson <geilsonsrz@gmail.com>
2024-01-21 18:52:17 -0300

    Estilos botoes

--- src/scripts/efeitos_estilos/figura.js
+++ src/scripts/efeitos_estilos/figura.js
@@ -4,6 +4,16 @@
 
 function figura() {
 
+    // ID da fatia
+    const ids = {
+        6: 'fatia-superficie',
+        5: 'fatia-04m',
+        4: 'fatia-08m',
+        3: 'fatia-12m',
+        2: 'fatia-16m',
+        1: 'fatia-20m'
+    }
+
     // Capturando a área da figura
     const area_figura = document.getElementById('area-figura')
 
@@ -12,17 +22,16 @@ function figura() {
     objeto.classList.add('figura')
 
     // Loop para adicionar os elementos filhos
-    for (let n=10; n>0; n--) {
+    for (let n=6; n>0; n--) {
 
-
-
-        
+        // Criando as fatias
         const fatia = document.createElement('span')
+        fatia.classList.add('fatia')
         fatia.style = `--i:${n}`
-        fatia.innerText = `${n}`
+        fatia.id = ids[n]
 
+        // Adicionando a fatia no objeto
         objeto.appendChild(fatia)
-
     }
 
     // Adicionando o objeto/figura na tela
