7c0c04853c45e275502b20b25fc14581245e5427
Geilson <geilsonsrz@gmail.com>
2024-01-25 16:30:39 -0300

    Erro ao carregar a imagem pra download

--- src/scripts/processamento/f_downloadSVG.js
+++ src/scripts/processamento/f_downloadSVG.js
@@ -2,41 +2,37 @@
     CRIAÇÃO DO BOTÃO DE DOWNLOAD
 `
 
-
 function baixarGrafico(id_area) {
 
     // Identificação da área
     const area = document.getElementById(id_area)
 
     // Criação
-    const botao_download = document.createElement('span')
+    const botao_download = document.createElement('a')
     botao_download.classList.add('btn', 'btn-download')
     botao_download.innerText = 'Download'
 
     // Função para download
-    botao_download.addEventListener('click', ()=>{
-        
+    botao_download.addEventListener('click', () => {
+
         // Captura dos dados do SVG
-        const svg = document.getElementById(id_area).querySelector('svg').outerHTML
+        const svg_txt = document.getElementById(id_area).querySelector('svg').outerHTML;
+
+        // Converte string para SVG
+        const svg = new SVG(svg_txt);
+
+        // Converte o SVG em uma imagem PNG
+        const png_dados_url = svg.toDataURL('image/png');
 
 
-        // Criação do elemento canvas
-        let canvas = document.createElement('canvas')
-        
-
-
-        let largura_svg = svg.offsetWidth
-        let altura_svg = svg.offsetHeight
-
-        console.log(typeof(svg))
-        
-        
-
-    })
+        // Cria um link para baixar a imagem PNG
+        botao_download.href = png_dados_url;
+        botao_download.download = `${id_area}.png`;
+    });
 
     // Adicionando o botão na área
     area.appendChild(botao_download)
-    
+
 }
 
 
