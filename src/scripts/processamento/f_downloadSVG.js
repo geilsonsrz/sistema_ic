0ed40fce49023d06dc5cea5a2ede7f72130badf7
Geilson <geilsonsrz@gmail.com>
2024-01-28 21:52:35 -0300

    Falta ajeitar a parte do grafico inferior que esta cortando

--- src/scripts/processamento/f_downloadSVG.js
+++ src/scripts/processamento/f_downloadSVG.js
@@ -20,42 +20,53 @@ function baixarGrafico(id_area) {
     botao_download.classList.add('btn', 'btn-download')
     botao_download.innerText = 'Download'
 
-    // Função para download
-    botao_download.addEventListener('click', () => {
+    // Capturando o SVG da área
+    let svg
+    for (let filho in area.children) {
+        if (area.children[filho].tagName === 'svg') { svg = area.children[filho] }
+    }
 
-        // Capturando o SVG da área
-        let svg
-        for (let filho in area.children) {
-            if (area.children[filho].tagName === 'svg') { svg = area.children[filho] }
-        }
+    // Criando a imagem do gráfico
+    const svg_img = new Image();
+    const svg_txt = new XMLSerializer().serializeToString(svg);
+    const blob = new Blob([svg_txt], { type: 'image/svg+xml' });
+    const url = URL.createObjectURL(blob);
 
-        const svg_img = new Image();
-        const svg_txt = new XMLSerializer().serializeToString(svg);
-        const blob = new Blob([svg_txt], { type: 'image/svg+xml' });
-        const url = URL.createObjectURL(blob);
+    // Carregando o gráfico na imagem
+    svg_img.onload = function () {
 
-        `
-            IMAGEM DO GRÁFICO TÁ SENDO CORTADA NA PARTE DA LEGENDA INFERIOR
-        `
-        svg_img.onload = function () {
-            const canvas = document.createElement('canvas');
-            canvas.width = svg.width.baseVal.value;
-            canvas.height = svg.height.baseVal.value;
-            const ctx = canvas.getContext('2d');
-            ctx.drawImage(svg_img, 0, 0);
-            const data_url = canvas.toDataURL('image/png');
+        // Criando e configurando o elemento canvas
+        const canvas = document.createElement('canvas');
+        canvas.width = svg.width.baseVal.value
+        canvas.height = svg.height.baseVal.value
 
-            // Download do gráfico
-            botao_download.href = data_url
-            botao_download.download = `${id_area}.png`
-            
-        };
-        
-        svg_img.src = url;
-    });
-    
+        // Configuração do contexto
+        const ctx = canvas.getContext('2d')
+
+        // Adiciona um retângulo como cor de fundo
+        ctx.fillStyle = 'white'; // Cor de fundo desejada
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        // Desenhando a imagem
+        ctx.drawImage(svg_img, 0, 0)
+
+        // Configuração da URL
+        const data_url = canvas.toDataURL('image/png')
+        botao_download.href = data_url
+    };
+
+    // Atribuindo a url no source do botão
+    svg_img.src = url;
+
     // Adicionando o botão na área
     area.appendChild(botao_download)
+
+    // Detecção do clique para download
+    botao_download.addEventListener('click', () => {
+
+        botao_download.download = `${id_area}.png`
+
+    });
 
 }
 
